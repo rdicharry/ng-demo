5edaaa365401be20285691a413e9450155235c13
Rebecca Dicharry <rdicharry@gmail.com>
2016-11-19 15:26:00 -0700

    added weather template weather.php

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="jumbotron text-center">
 			<h1>Welcome to the Angular 2 Demo!</h1>
-			<p class="lead">This is a bare-bones Angular 2 Front End :D</p>
+			<p class="lead">This is a "simple" lol app to display weather data. Enjoy!</p>
 		</div>
 	</div>
 </section>
---
+++ public_html/templates/weather.php
@@ -0,0 +1,6 @@
+<ul>
+	<li>Min Temp: {{weather.temperatureMin}} degF</li>
+	<li>Max Temp: {{weather.temperatureMax}} degF</li>
+	<li>Wind Speed: {{weather.windSpeed}} mph</li>
+	<li>Timestamp: {{weather.timestamp}} s</li>
+</ul>
